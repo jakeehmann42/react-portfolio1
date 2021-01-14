a9d972bf961842ca7061b16de806a9acdbf757a7
Jakeehmann42 <Jakeehmann42@gmail.com>
2021-01-13 18:45:23 -0700

    Inserted info from About me page.

---
+++ my-app/src/components/About.js
@@ -0,0 +1,12 @@
+const About = () => (
+    <div>
+        <h2>About Myself</h2>
+        <p>
+            Hello World! My name is Jake Ehmann from Mesa, Arizona. 
+            A junior web developer currently taking courses in Full Stack Development at
+            the University of Arizona. I currently work as a lockbox employee at CIT bank located in Phoenix.
+        </p>
+    </div>
+);
+
+export default About;
